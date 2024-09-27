0f125fd594ccd075f050645869a3defefaf21fe0
monjur <monjurmorshed0231@gmail.com>
2024-09-27 16:28:17 +0900

    Added azure pipeline

--- src/main/java/com/monjur/jobApp/controller/JobController.java
+++ src/main/java/com/monjur/jobApp/controller/JobController.java
@@ -2,6 +2,8 @@ package com.monjur.jobApp.controller;
 
 import com.monjur.jobApp.model.JobPost;
 import com.monjur.jobApp.service.JobService;
+import java.security.SecureRandom;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,20 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.SecureRandom;
-import java.util.List;
-
 @Controller
 public class JobController {
-
   @Autowired
   private JobService service;
 
   /*--------- For Home ------------------------*/
-  @GetMapping({"/", "/home"})
-  public String home(){
+  @GetMapping({ "/", "/home" })
+  public String home() {
     return "home";
   }
+
   /*--------- For Home ------------------------*/
 
   /*--------- For Add Job ---------------------*/
@@ -30,22 +29,22 @@ public class JobController {
   public String addJob() {
     return "addjob";
   }
-  /*--------- For Add Job ----------------------*/
 
+  /*--------- For Add Job ----------------------*/
 
   /*--------- For Fetching All Jobs ------------*/
   @GetMapping("/viewalljobs")
-  public String viewJobs(Model model){
+  public String viewJobs(Model model) {
     List<JobPost> jobPosts = service.returnAllJobPosts();
     model.addAttribute("jobPosts", jobPosts);
     return "viewalljobs";
   }
-  /*--------- For Fetching All Jobs ------------*/
 
+  /*--------- For Fetching All Jobs ------------*/
 
   /*--------- For Adding New Jobs --------------*/
   @PostMapping("handleForm")
-  public String handleForm(JobPost jobPost,Model model){
+  public String handleForm(JobPost jobPost, Model model) {
     model.addAttribute("jobPost", jobPost);
     service.addJobPost(jobPost);
     return "success";
